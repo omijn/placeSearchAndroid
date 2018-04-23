e63bdca9fc237bf916627cc9042da923cce48e12
omijn <abhijit.kashyap07@gmail.com>
2018-04-23 10:30:39 -0700

    sort reviews

--- app/src/main/java/com/example/omijn/placeandentertainmentsearch/Review.java
+++ app/src/main/java/com/example/omijn/placeandentertainmentsearch/Review.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Review {
+    private int index; // used to determine order in which reviews were retrieved from Google, so we can do a default sort later
     private String review;
     private String author;
     private String reviewLink;
@@ -11,13 +12,18 @@ public class Review {
     private String timestamp;
     private Double rating;
 
-    public Review(String review, String author, String reviewLink, String authorPhoto, String timestamp, Double rating) {
+    public Review(int i, String review, String author, String reviewLink, String authorPhoto, String timestamp, Double rating) {
+        this.index = i;
         this.review = review;
         this.author = author;
         this.reviewLink = reviewLink;
         this.authorPhoto = authorPhoto;
         this.timestamp = timestamp;
         this.rating = rating;
+    }
+
+    public int getIndex() {
+        return index;
     }
 
     public String getReview() {
@@ -40,6 +46,10 @@ public class Review {
         Date date = new Date(Long.parseLong(timestamp) * 1000);
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return dateFormat.format(date);
+    }
+
+    public Long getUnixTimestamp() {
+        return Long.parseLong(timestamp);
     }
 
     public float getRating() {
--- app/src/main/java/com/example/omijn/placeandentertainmentsearch/ReviewsFragment.java
+++ app/src/main/java/com/example/omijn/placeandentertainmentsearch/ReviewsFragment.java
@@ -10,7 +10,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -19,6 +21,8 @@ import org.json.JSONObject;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 /**
@@ -28,6 +32,8 @@ public class ReviewsFragment extends Fragment implements ReviewsAdapter.ListItem
 
     private ArrayList<Review> reviewsData;
     private ReviewsAdapter adapter;
+
+    private Spinner sortSpinner;
 
     public ReviewsFragment() {
         // Required empty public constructor
@@ -54,7 +60,7 @@ public class ReviewsFragment extends Fragment implements ReviewsAdapter.ListItem
                     String reviewText = review.optString("text", "");
                     String reviewLink = review.optString("author_url", "");
 
-                    reviewsData.add(new Review(reviewText, authorName, reviewLink, authorPhotoLink, timestamp, rating));
+                    reviewsData.add(new Review(i, reviewText, authorName, reviewLink, authorPhotoLink, timestamp, rating));
                 }
             }
 
@@ -64,10 +70,40 @@ public class ReviewsFragment extends Fragment implements ReviewsAdapter.ListItem
 
         RecyclerView recyclerView = v.findViewById(R.id.da_rf_rv_reviews);
         TextView emptyView = v.findViewById(R.id.da_rf_tv_no_reviews);
+        sortSpinner = v.findViewById(R.id.da_rf_sp_sort);
 
         adapter = new ReviewsAdapter(reviewsData, this);
 
         recyclerView.setAdapter(adapter);
+
+        sortSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        defaultSort();
+                        break;
+                    case 1:
+                        rateSort(-1);   // descending (highest rating first)
+                        break;
+                    case 2:
+                        rateSort(1);    // ascending (lowest rating first)
+                        break;
+                    case 3:
+                        dateSort(-1);   // descending (newest review first)
+                        break;
+                    case 4:
+                        dateSort(1);    // ascending (oldest review first)
+                        break;
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(v.getContext());
         recyclerView.setLayoutManager(layoutManager);
@@ -93,4 +129,50 @@ public class ReviewsFragment extends Fragment implements ReviewsAdapter.ListItem
         }
 
     }
+
+    public void rateSort(final int order) {
+        Collections.sort(reviewsData, new Comparator<Review>() {
+            @Override
+            public int compare(Review r1, Review r2) {
+                if (r1.getRating() > r2.getRating()) {
+                    return order;
+                } else if (r1.getRating() < r2.getRating()) {
+                    return -1 * order;
+                } else {
+                    return 0;
+                }
+            }
+        });
+    }
+
+    public void dateSort(final int order) {
+        Collections.sort(reviewsData, new Comparator<Review>() {
+            @Override
+            public int compare(Review r1, Review r2) {
+                if (r1.getUnixTimestamp() > r2.getUnixTimestamp()) {
+                    return order;
+                } else if (r1.getUnixTimestamp() < r2.getUnixTimestamp()) {
+                    return -1 * order;
+                } else {
+                    return 0;
+                }
+            }
+        });
+    }
+
+    public void defaultSort() {
+        Collections.sort(reviewsData, new Comparator<Review>() {
+            @Override
+            public int compare(Review r1, Review r2) {
+                if (r1.getIndex() > r2.getIndex()) {
+                    return 1;
+                } else if (r1.getIndex() < r2.getIndex()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+    }
+
 }
